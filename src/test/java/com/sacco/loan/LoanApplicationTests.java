2c37aa20de602f138b5f675eae9daa9411360642
Tendo Martin <tmkmart@gmail.com>
2024-10-27 18:08:30 +0300

    Add more changes

---
+++ src/test/java/com/sacco/loan/LoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sacco.loan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
